47b8c5adeef7a9964847ff07451da1e4d37eb0c3
Elmo Junior Ficagna <elmojrf@gmail.com>
2019-06-03 22:15:50 -0400

    Adicionadas validações ao model User

--- model/users/User.js
+++ model/users/User.js
@@ -8,18 +8,59 @@ const Sequelize = sequelize.Sequelize;
 const Token = require('./Token');
 
 //Definição do Sequelize Model
-const User = sequelize.define('user',{
+const User = sequelize.define('user', {
     login: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            notNull: {
+                msg: 'O login é obrigatório.'
+            },
+            len: {
+                args: [5, 10],
+                msg: 'O login deve ter entre 5 e 10 caracteres.'
+            }
+        }
     },
     email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            notNull: {
+                msg: 'O email é obrigatório.'
+            },
+            isEmail: {
+                msg: 'O email informado não é válido'
+            }
+        }
     },
     name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+                msg: 'O nome é obrigatório.'
+            },
+            len: {
+                args: [5, 120],
+                msg: 'O nome deve ter entre 5 e 120 caracteres.'
+            }
+        }
     },
     password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notNull: {
+                msg: 'A senha é obrigatória.'
+            },
+            len: {
+                args: [5, 10],
+                msg: 'A senha deve ter entre 5 e 10 caracteres.'
+            }
+        }
     }
 })
 
--- scripts/sample-data.js
+++ scripts/sample-data.js
@@ -3,21 +3,21 @@ const moment = require('moment');
 const users = [{
         id: 1,
         login: 'elmo',
-        email: 'elmo@provedor.com ',
+        email: 'elmo@provedor.com',
         name: 'Elmo Junior Ficagna',
         password: 'teste'
     },
     {
         id: 2,
         login: 'felipe',
-        email: 'felipe@provedor.com ',
+        email: 'felipe@provedor.com',
         name: 'Felipe Caggi',
         password: 'teste'
     },
     {
         id: 3,
         login: 'hueligton',
-        email: 'hueligton@provedor.com ',
+        email: 'hueligton@provedor.com',
         name: 'Hueligton Pereira de Melo',
         password: 'teste'
     }
